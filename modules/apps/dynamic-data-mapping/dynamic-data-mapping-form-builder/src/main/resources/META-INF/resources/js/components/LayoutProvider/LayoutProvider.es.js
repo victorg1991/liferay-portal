020791fb2882596737518b7c2835c152a720ad43
Bruno Basto <bruno.basto@liferay.com>
2020-02-18 18:55:32 -0300

    LPS-98077 Allows deleting a nested field

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
@@ -277,7 +277,7 @@ class LayoutProvider extends Component {
 				label: Liferay.Language.get('duplicate')
 			},
 			{
-				action: indexes => this.dispatch('fieldDeleted', {indexes}),
+				action: fieldName => this.dispatch('fieldDeleted', {fieldName}),
 				label: Liferay.Language.get('delete')
 			}
 		];
@@ -355,7 +355,7 @@ class LayoutProvider extends Component {
 	}
 
 	_handleFieldDeleted(event) {
-		this.setState(handleFieldDeleted(this.state, event));
+		this.setState(handleFieldDeleted(this.props, this.state, event));
 	}
 
 	_handleFieldDuplicated(event) {
