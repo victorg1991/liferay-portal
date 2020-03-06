473082c24a4d8728af67ec2fd39f2e15a4973ebe
Sandro Chinea <sandrochinea@gmail.com>
2020-03-06 11:44:30 +0100

    Revert "LPS-109725 Wrap contents of collection in the ControlsIdConverteContextProvider..."
    This reverts commit 4e11175cb8eccba04b91d57c2c16406b44b91219.

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Collection.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Collection.js
@@ -14,60 +14,16 @@
 
 import React from 'react';
 
-import {ControlsIdConverterContextProvider} from '../ControlsIdConverterContext';
-
-const COLLECTION_ID_DIVIDER = '$';
-
-function getCollectionPrefix(collectionId, index) {
-	return `collection-${collectionId}-${index}${COLLECTION_ID_DIVIDER}`;
-}
-
-function getToControlsId(collectionId, index) {
-	return itemId => {
-		if (!itemId) {
-			return null;
-		}
-
-		return `${getCollectionPrefix(collectionId, index)}${itemId}`;
-	};
-}
-
-function fromControlsId(controlsItemId) {
-	if (!controlsItemId) {
-		return null;
-	}
-
-	const [, itemId] = controlsItemId.split(COLLECTION_ID_DIVIDER);
-
-	return itemId;
-}
-
 const NotMappedMessage = () => (
 	<div className="page-editor__collection__not-mapped-message">
 		{Liferay.Language.get('not-mapped')}
 	</div>
 );
 
-const Collection = React.forwardRef(({children, item}, ref) => {
-	if (React.Children.count(children) == 0) {
-		return <NotMappedMessage />;
-	}
-
-	const child = React.Children.only(children);
-
+const Collection = React.forwardRef(({_props}, ref) => {
 	return (
 		<div className="page-editor__collection" ref={ref}>
-			{Array.from({length: 3}).map((_element, idx) => (
-				<ControlsIdConverterContextProvider
-					key={idx}
-					value={{
-						fromControlsId,
-						toControlsId: getToControlsId(item.itemId, idx),
-					}}
-				>
-					{React.cloneElement(child)}
-				</ControlsIdConverterContextProvider>
-			))}
+			<NotMappedMessage></NotMappedMessage>
 		</div>
 	);
 });
