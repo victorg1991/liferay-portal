569b6287ab7484f0403464536d3f279982c524d0
Victor Galan <victor.galan@liferay.com>
2020-03-10 16:55:23 +0100

    LPS-109777 Include name in page structure aswell

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/PageStructureSidebar.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/PageStructureSidebar.js
@@ -92,6 +92,9 @@ function getName(item, fragmentEntryLinks) {
 	if (item.type === LAYOUT_DATA_ITEM_TYPES.fragment) {
 		name = fragmentEntryLinks[item.config.fragmentEntryLinkId].name;
 	}
+	else if (item.type === LAYOUT_DATA_ITEM_TYPES.collection) {
+		name = LAYOUT_DATA_ITEM_TYPE_LABELS.collection;
+	}
 	else if (item.type === LAYOUT_DATA_ITEM_TYPES.container) {
 		name = LAYOUT_DATA_ITEM_TYPE_LABELS.container;
 	}
