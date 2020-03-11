c788f1e2780cbddb8a8eab29eb7402dc956a0703
Sandro Chinea <sandrochinea@gmail.com>
2020-03-11 10:14:09 +0000

    LPS-109777 Don't show collection items in structure tree

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/PageStructureSidebar.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/PageStructureSidebar.js
@@ -87,7 +87,12 @@ export default function PageStructureSidebar() {
 			});
 		}
 		else {
-			item.children.forEach(childId => {
+			const itemChildren =
+				item.type === LAYOUT_DATA_ITEM_TYPES.collection
+					? items[item.children[0]].children
+					: item.children;
+
+			itemChildren.forEach(childId => {
 				const childItem = items[childId];
 
 				if (
@@ -122,6 +127,7 @@ export default function PageStructureSidebar() {
 	};
 
 	const data = masterLayoutData || layoutData;
+
 	const nodes = visit(data.items[data.rootItems.main], data.items).children;
 
 	return (
