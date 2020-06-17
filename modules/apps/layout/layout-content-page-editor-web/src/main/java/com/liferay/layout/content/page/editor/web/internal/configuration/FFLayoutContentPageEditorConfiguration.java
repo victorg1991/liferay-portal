e44d530a79906b656c56088b8ac110252f286146
Pablo Molina <contact@pablomolina.me>
2020-06-17 08:33:13 +0200

    ff

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/configuration/FFLayoutContentPageEditorConfiguration.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/configuration/FFLayoutContentPageEditorConfiguration.java
@@ -30,7 +30,7 @@ public interface FFLayoutContentPageEditorConfiguration {
 	@Meta.AD(deflt = "false", required = false)
 	public boolean containerItemEnabled();
 
-	@Meta.AD(deflt = "false", required = false)
+	@Meta.AD(deflt = "true", required = false)
 	public boolean responsiveEnabled();
 
 }
