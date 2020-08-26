8377d3c26d640eb03361e579dd8a89a2e9edfc63
Sandro Chinea <sandrochinea@gmail.com>
2020-08-26 16:22:17 +0100

    [PENDING] LPS-119634 Move common styles to FragmentContent

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/UnsafeHTML.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/UnsafeHTML.js
@@ -108,6 +108,12 @@ export default class UnsafeHTML extends React.PureComponent {
 	_syncRefProps() {
 		const ref = this.state.ref;
 		ref.className = this.props.className;
+
+		//ref.style = this.props.style;
+
+		Object.keys(this.props.style).forEach((key) => {
+			ref.style[key] = this.props.style[key];
+		});
 	}
 
 	/**
@@ -164,6 +170,7 @@ UnsafeHTML.defaultProps = {
 	},
 	markup: '',
 	onRender: () => {},
+	style: {},
 };
 
 UnsafeHTML.propTypes = {
@@ -180,4 +187,5 @@ UnsafeHTML.propTypes = {
 	}),
 	markup: PropTypes.string,
 	onRender: PropTypes.func,
+	style: PropTypes.object,
 };
