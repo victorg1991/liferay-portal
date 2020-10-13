5b59811831b3e695db35bbf750f8d8bd3fc632bd
Sandro Chinea <sandrochinea@gmail.com>
2020-10-13 08:40:36 +0100

    LPS-122017 Avoid getting empty object as image url

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/item-configuration-panels/ImagePropertiesPanel.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/item-configuration-panels/ImagePropertiesPanel.js
@@ -106,7 +106,11 @@ export function ImagePropertiesPanel({item}) {
 			processorKey
 		);
 
-		return content?.url ?? content;
+		if (typeof content === 'string' || content?.url) {
+			return content?.url ?? content;
+		}
+
+		return '';
 	});
 
 	const imageTitle =
