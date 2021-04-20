68a944a8546183884f20a31c6a378ad54ef32edd
Victor Galan <victor.galan@liferay.com>
2021-04-20 09:15:36 +0200

    Add classTypeId

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -1806,6 +1806,9 @@ public class ContentPageEditorDisplayContext {
 				).put(
 					"classPK", layoutDisplayPageObjectProvider.getClassPK()
 				).put(
+					"classTypeId",
+					layoutDisplayPageObjectProvider.getClassTypeId()
+				).put(
 					"title",
 					layoutDisplayPageObjectProvider.getTitle(
 						themeDisplay.getLocale())
