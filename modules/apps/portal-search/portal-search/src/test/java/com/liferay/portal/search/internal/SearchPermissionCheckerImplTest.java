0aa4a19b6bc0b5f449ffd56f0399a9ebce24339c
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-12 11:06:02 +0200

    LPS-134885 JUnit tests

--- modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/SearchPermissionCheckerImplTest.java
+++ modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/SearchPermissionCheckerImplTest.java
@@ -14,20 +14,28 @@
 
 package com.liferay.portal.search.internal;
 
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.BooleanClause;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistry;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchPermissionChecker;
 import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.UserBag;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.search.configuration.SearchPermissionCheckerConfiguration;
+import com.liferay.portal.search.spi.model.permission.SearchPermissionFilterContributor;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -35,6 +43,7 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -69,6 +78,112 @@ public class SearchPermissionCheckerImplTest {
 		Assert.assertNull(
 			_searchPermissionChecker.getPermissionBooleanFilter(
 				0, null, 0, null, null, null));
+	}
+
+	@Test
+	public void testPermissionFilterMultipleResourcePermissionName()
+		throws Exception {
+
+		long userId = RandomTestUtil.randomLong();
+
+		whenIndexerIsPermissionAware(true);
+		whenPermissionCheckerGetUser(_user);
+		whenPermissionCheckerGetUserBag(_userBag);
+		whenUserGetUserId(userId);
+
+		ReflectionTestUtil.getAndSetFieldValue(
+			_searchPermissionChecker, "_searchPermissionFilterContributors",
+			Arrays.asList(_searchPermissionFilterContributor));
+
+		SearchContext searchContext = new SearchContext();
+		String resourceName1 = StringBundler.concat(
+			"com.liferay.asset.kernel.model.AssetCategory-",
+			"com.liferay.dynamic.data.mapping.model.DDMTemplate");
+		String resourceName2 = StringBundler.concat(
+			"com.liferay.dynamic.data.mapping.model.DDMTemplate-",
+			"com.liferay.journal.model.JournalArticle");
+
+		searchContext.setAttribute(
+			"resourcePermissionNames",
+			new String[] {resourceName1, resourceName2});
+
+		BooleanFilter booleanFilter = null;
+
+		BooleanFilter permissionBooleanFilter =
+			_searchPermissionChecker.getPermissionBooleanFilter(
+				0, null, userId, null, booleanFilter, searchContext);
+
+		Assert.assertNotNull(permissionBooleanFilter);
+
+		List<BooleanClause<Filter>> shouldBooleanClauses =
+			permissionBooleanFilter.getShouldBooleanClauses();
+
+		Assert.assertEquals(
+			shouldBooleanClauses.toString(), 2, shouldBooleanClauses.size());
+
+		Mockito.verify(
+			_searchPermissionFilterContributor
+		).contribute(
+			Matchers.any(BooleanFilter.class), Matchers.eq((long)0),
+			Matchers.eq(null), Matchers.eq(userId),
+			Matchers.eq(_permissionChecker), Matchers.eq(resourceName1)
+		);
+
+		Mockito.verify(
+			_searchPermissionFilterContributor
+		).contribute(
+			Matchers.any(BooleanFilter.class), Matchers.eq((long)0),
+			Matchers.eq(null), Matchers.eq(userId),
+			Matchers.eq(_permissionChecker), Matchers.eq(resourceName2)
+		);
+
+		Mockito.verifyNoMoreInteractions(_searchPermissionFilterContributor);
+	}
+
+	@Test
+	public void testPermissionFilterSingleResourcePermissionName()
+		throws Exception {
+
+		long userId = RandomTestUtil.randomLong();
+
+		whenIndexerIsPermissionAware(true);
+		whenPermissionCheckerGetUser(_user);
+		whenPermissionCheckerGetUserBag(_userBag);
+		whenUserGetUserId(userId);
+
+		ReflectionTestUtil.getAndSetFieldValue(
+			_searchPermissionChecker, "_searchPermissionFilterContributors",
+			Arrays.asList(_searchPermissionFilterContributor));
+
+		SearchContext searchContext = new SearchContext();
+		String resourceName = StringBundler.concat(
+			"com.liferay.asset.kernel.model.AssetCategory-",
+			"com.liferay.dynamic.data.mapping.model.DDMTemplate");
+
+		searchContext.setAttribute("resourcePermissionName", resourceName);
+
+		BooleanFilter booleanFilter = null;
+
+		BooleanFilter permissionBooleanFilter =
+			_searchPermissionChecker.getPermissionBooleanFilter(
+				0, null, userId, null, booleanFilter, searchContext);
+
+		Assert.assertNotNull(permissionBooleanFilter);
+
+		List<BooleanClause<Filter>> shouldBooleanClauses =
+			permissionBooleanFilter.getShouldBooleanClauses();
+
+		Assert.assertEquals(
+			shouldBooleanClauses.toString(), 1, shouldBooleanClauses.size());
+
+		Mockito.verify(
+			_searchPermissionFilterContributor
+		).contribute(
+			permissionBooleanFilter, 0, null, userId, _permissionChecker,
+			resourceName
+		);
+
+		Mockito.verifyNoMoreInteractions(_searchPermissionFilterContributor);
 	}
 
 	@Test
@@ -160,6 +275,10 @@ public class SearchPermissionCheckerImplTest {
 	@Mock
 	private SearchPermissionCheckerConfiguration
 		_searchPermissionCheckerConfiguration;
+
+	@Mock
+	private SearchPermissionFilterContributor
+		_searchPermissionFilterContributor;
 
 	@Mock
 	private User _user;
