d14c8ebdca073e2dcd5337f8e5fd7969931e1c34
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-12 12:22:48 +0200

    LPS-134885 Add DDMTemplatePermissionSupport to Blogs

---
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/security/permission/BlogsEntryDDMTemplatePermissionSupport.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.web.internal.security.permission;
+
+import com.liferay.blogs.constants.BlogsConstants;
+import com.liferay.dynamic.data.mapping.util.DDMTemplatePermissionSupport;
+
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.service.ResourceActionLocalServiceUtil;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.List;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.blogs.model.BlogsEntry",
+	service = {
+		BlogsEntryDDMTemplatePermissionSupport.class,
+		DDMTemplatePermissionSupport.class
+	}
+)
+public class BlogsEntryDDMTemplatePermissionSupport
+	implements DDMTemplatePermissionSupport {
+
+	@Override
+	public String getResourceName(long classNameId) {
+		return _RESOURCE_NAME;
+	}
+
+	@Activate
+	protected void activate() {
+		List<String> actions = ResourceActionsUtil.getModelResourceActions(_RESOURCE_NAME);
+		if (actions.isEmpty()) {
+			ResourceActionLocalServiceUtil.addResourceAction(_RESOURCE_NAME, ActionKeys.DELETE, 2);
+			ResourceActionLocalServiceUtil.addResourceAction(_RESOURCE_NAME, ActionKeys.PERMISSIONS, 4);
+			ResourceActionLocalServiceUtil.addResourceAction(_RESOURCE_NAME, ActionKeys.UPDATE, 8);
+			ResourceActionLocalServiceUtil.addResourceAction(_RESOURCE_NAME, ActionKeys.VIEW, 1);
+		}
+	}
+
+	private final static String _RESOURCE_NAME =
+			"com.liferay.blogs.model.BlogsEntry-"
+					+ "com.liferay.dynamic.data.mapping.model.DDMTemplate";
+
+
+}
