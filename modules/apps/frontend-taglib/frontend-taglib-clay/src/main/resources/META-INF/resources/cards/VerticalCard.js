a045a42967c39ba81f503f2df9a4836d91c447a0
Victor Galan <victor.galan@liferay.com>
2022-11-14 11:33:19 +0100

    LPS-XXX Check and update selection from react
    This should be applied to all the cards

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/cards/VerticalCard.js
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/cards/VerticalCard.js
@@ -15,7 +15,7 @@
 import {ClayCardWithInfo} from '@clayui/card';
 import ClayIcon from '@clayui/icon';
 import ClaySticker from '@clayui/sticker';
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 
 import {EVENT_MANAGEMENT_TOOLBAR_TOGGLE_ALL_ITEMS} from '../constants';
 import getDataAttributes from '../get_data_attributes';
@@ -57,6 +57,8 @@ export default function VerticalCard({
 	const normalizedActions = useMemo(() => normalizeDropdownItems(actions), [
 		actions,
 	]);
+
+	const cardRef = useRef();
 
 	const [selected, setSelected] = useState(initialSelected);
 
@@ -112,6 +114,49 @@ export default function VerticalCard({
 		};
 	}, [handleToggleAllItems]);
 
+	useEffect(() => {
+		const checkSelection = () => {
+			const searchContainerContent = document.querySelector(
+				'.searchcontainer-content'
+			);
+
+			if (!searchContainerContent?.dataset.selectionData) {
+				return;
+			}
+
+			const selectionData = JSON.parse(
+				searchContainerContent.dataset.selectionData
+			);
+
+			const state = selectionData.find(
+				(data) => data.value === inputValue
+			);
+
+			if (state?.checked) {
+				setSelected(true);
+			}
+		};
+
+		// Here the AUI component may be loaded or not, in practice it was only loaded when the last card loaded
+		// this is why we check if the searchcontainer is loaded, otherwise we listen for the event
+
+		const searchContainer = document.querySelector('.searchcontainer')?.id;
+
+		if (searchContainer) {
+			checkSelection();
+		}
+		else {
+			Liferay.on('search-container:registered', () => {
+				requestAnimationFrame(checkSelection);
+			});
+		}
+
+		checkSelection();
+
+		return () =>
+			Liferay.detach('search-container:registered', checkSelection);
+	}, [inputValue]);
+
 	return (
 		<ClayCardWithInfo
 			actions={normalizedActions}
@@ -151,6 +196,7 @@ export default function VerticalCard({
 					  }
 					: null
 			}
+			ref={cardRef}
 			selectable={selectable}
 			selected={selected}
 			stickerProps={showSticker ? stickerProps : null}
