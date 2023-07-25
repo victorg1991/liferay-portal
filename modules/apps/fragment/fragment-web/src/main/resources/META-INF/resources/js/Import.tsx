525b471b00c3b11a9d50665b73676dbfb38fdc37
clara izquierdo <claraizquierdosanz@gmail.com>
2023-07-25 16:58:10 +0200

    LPS-190594 SF

--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/Import.tsx
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/Import.tsx
@@ -18,18 +18,13 @@ import ClayIcon from '@clayui/icon';
 import ClayLayout from '@clayui/layout';
 import ClayToolbar from '@clayui/toolbar';
 import classNames from 'classnames';
-import {
-    fetch,
-	navigate,
-    openToast,
-    sub
-} from 'frontend-js-web';
+import {fetch, navigate, openToast, sub} from 'frontend-js-web';
 import React, {useState} from 'react';
 
-import ImportResults, { ImportResultsData } from './ImportResults';
+import ImportResults, {ImportResultsData} from './ImportResults';
 
 interface Props {
-    backURL: string,
+	backURL: string;
 	importURL: string;
 	portletNamespace: string;
 }
@@ -38,24 +33,27 @@ function Import({backURL, importURL, portletNamespace}: Props) {
 	const [error, setError] = useState('');
 	const [isValidForm, setIsValidForm] = useState(false);
 	const [overwrite, setOverwrite] = useState(true);
-    const [file, setFile] = useState<File | null>(null);
-    const [fileName, setFileName] = useState<string | null>(null)
-    const [importResults, setImportResults] = useState<ImportResultsData | null>(null)
+	const [file, setFile] = useState<File | null>(null);
+	const [fileName, setFileName] = useState<string | null>(null);
+	const [
+		importResults,
+		setImportResults,
+	] = useState<ImportResultsData | null>(null);
 
 	const validateFile = (event: React.ChangeEvent<HTMLInputElement>) => {
 		if (!event.target.files || event.target.files?.length === 0) {
 			setIsValidForm(false);
-            setFile(null);
-            setFileName(null);
+			setFile(null);
+			setFileName(null);
 
 			return;
 		}
 
-        setFile(event.target.files[0]);
+		setFile(event.target.files[0]);
 
 		const fileName: string = event.target.files[0]?.name || '';
 
-        setFileName(fileName);
+		setFileName(fileName);
 
 		const fileExtension = fileName
 			.substring(fileName.lastIndexOf('.') + 1)
@@ -71,155 +69,189 @@ function Import({backURL, importURL, portletNamespace}: Props) {
 		}
 	};
 
-    const goBack = () => {
-        navigate(backURL);
-    }
+	const goBack = () => {
+		navigate(backURL);
+	};
 
-    const importOtherFile = () => {
-        setImportResults(null);
-    }
+	const importOtherFile = () => {
+		setImportResults(null);
+	};
 
-    const importFile = () => {
-        const formData = new FormData();
+	const importFile = () => {
+		const formData = new FormData();
 
-        if (!file) {
-            return;
-        }
+		if (!file) {
+			return;
+		}
 
 		formData.append(`${portletNamespace}file`, file);
-        formData.append(`${portletNamespace}overwrite`, overwrite.toString());
+		formData.append(`${portletNamespace}overwrite`, overwrite.toString());
 
 		fetch(importURL, {
 			body: formData,
 			method: 'POST',
 		})
-        .then((response) => response.json())
-        .then(({importResults}) => {
-                if (!importResults || (!importResults.imported && !importResults["imported-draft"] && !importResults.invalid)) {
-                    navigate(backURL);
-                    openToast({
-                        message: sub(
-                            Liferay.Language.get('something-went-wrong-and-the-x-could-not-be-imported'),
-                            fileName || ""
-                        ),
-                        type: 'danger',
-                    });
-                }
-                setImportResults(importResults);
-                setFile(null);
-                setIsValidForm(false);
-            })
+			.then((response) => response.json())
+			.then(({importResults}) => {
+				if (
+					!importResults ||
+					(!importResults.imported &&
+						!importResults['imported-draft'] &&
+						!importResults.invalid)
+				) {
+					navigate(backURL);
+					openToast({
+						message: sub(
+							Liferay.Language.get(
+								'something-went-wrong-and-the-x-could-not-be-imported'
+							),
+							fileName || ''
+						),
+						type: 'danger',
+					});
+				}
+				setImportResults(importResults);
+				setFile(null);
+				setIsValidForm(false);
+			})
 			.catch(() => {
 				openToast({
-                    message: sub(
-                        Liferay.Language.get('something-went-wrong-and-the-x-could-not-be-imported'),
-                        fileName || ""
-                    ),
+					message: sub(
+						Liferay.Language.get(
+							'something-went-wrong-and-the-x-could-not-be-imported'
+						),
+						fileName || ''
+					),
 					type: 'danger',
 				});
 			});
-    }
+	};
 
 	return (
-        <div className="fragment-import">
-            <ClayToolbar light>
-                <ClayLayout.ContainerFluid>
-                    <ClayToolbar.Nav className="justify-content-sm-end">
-                        {importResults ? (
-                            <>
-                                <ClayToolbar.Item>
-                                    <ClayButton displayType="secondary" onClick={importOtherFile} size="sm">{Liferay.Language.get('upload-another-file')}</ClayButton>
-                                </ClayToolbar.Item>
-        
-                                <ClayToolbar.Item>
-                                    <ClayButton onClick={goBack} size="sm">{Liferay.Language.get('done')}</ClayButton>
-                                </ClayToolbar.Item>
-                            </>
-                        ) : (
-                            <>
-                                <ClayToolbar.Item>
-                                    <ClayButton displayType="secondary" onClick={goBack} size="sm">{Liferay.Language.get('cancel')}</ClayButton>
-                                </ClayToolbar.Item>
+		<div className="fragment-import">
+			<ClayToolbar light>
+				<ClayLayout.ContainerFluid>
+					<ClayToolbar.Nav className="justify-content-sm-end">
+						{importResults ? (
+							<>
+								<ClayToolbar.Item>
+									<ClayButton
+										displayType="secondary"
+										onClick={importOtherFile}
+										size="sm"
+									>
+										{Liferay.Language.get(
+											'upload-another-file'
+										)}
+									</ClayButton>
+								</ClayToolbar.Item>
 
-                                <ClayToolbar.Item>
-                                    <ClayButton disabled={!isValidForm} onClick={importFile} size="sm">{Liferay.Language.get('import')}</ClayButton>
-                                </ClayToolbar.Item>
-                            </>
-                        
-                        )}
-                    </ClayToolbar.Nav> 
-                </ClayLayout.ContainerFluid>
-            </ClayToolbar>
-    
-            <ClayLayout.ContainerFluid view>
-                {importResults ? (
-                        <ImportResults fileName={fileName} importResults={importResults} />
-                ) : (
-                    <ClayLayout.Sheet size='lg'>
-                        <h2 className="c-mb-4 text-6">{Liferay.Language.get('import-file')}</h2>
+								<ClayToolbar.Item>
+									<ClayButton onClick={goBack} size="sm">
+										{Liferay.Language.get('done')}
+									</ClayButton>
+								</ClayToolbar.Item>
+							</>
+						) : (
+							<>
+								<ClayToolbar.Item>
+									<ClayButton
+										displayType="secondary"
+										onClick={goBack}
+										size="sm"
+									>
+										{Liferay.Language.get('cancel')}
+									</ClayButton>
+								</ClayToolbar.Item>
 
-                        <form
-                            action={importURL}
-                            data-fm-namespace={portletNamespace}
-                            encType="multipart/form-data"
-                            id={`${portletNamespace}fm`}
-                            method="post"
-                            name={`${portletNamespace}fm`}
-                        >
-                                <p>
-                                    {Liferay.Language.get(
-                                        'select-a-zip-file-containing-one-or-multiple-entries'
-                                    )}
-                                </p>
+								<ClayToolbar.Item>
+									<ClayButton
+										disabled={!isValidForm}
+										onClick={importFile}
+										size="sm"
+									>
+										{Liferay.Language.get('import')}
+									</ClayButton>
+								</ClayToolbar.Item>
+							</>
+						)}
+					</ClayToolbar.Nav>
+				</ClayLayout.ContainerFluid>
+			</ClayToolbar>
 
-                                <ClayForm.Group
-                                    className={classNames({'has-error': error})}
-                                >
-                                    <label htmlFor={`${portletNamespace}file`}>
-                                        {Liferay.Language.get('file')}
+			<ClayLayout.ContainerFluid view>
+				{importResults ? (
+					<ImportResults
+						fileName={fileName}
+						importResults={importResults}
+					/>
+				) : (
+					<ClayLayout.Sheet size="lg">
+						<h2 className="c-mb-4 text-6">
+							{Liferay.Language.get('import-file')}
+						</h2>
 
-                                        <ClayIcon
-                                            className="reference-mark"
-                                            symbol="asterisk"
-                                        />
-                                    </label>
+						<form
+							action={importURL}
+							data-fm-namespace={portletNamespace}
+							encType="multipart/form-data"
+							id={`${portletNamespace}fm`}
+							method="post"
+							name={`${portletNamespace}fm`}
+						>
+							<p>
+								{Liferay.Language.get(
+									'select-a-zip-file-containing-one-or-multiple-entries'
+								)}
+							</p>
 
-                                    <ClayInput
-                                        data-testid={`${portletNamespace}file`}
-                                        id={`${portletNamespace}file`}
-                                        name={`${portletNamespace}file`}
-                                        onChange={validateFile}
-                                        required
-                                        type="file"
-                                    />
+							<ClayForm.Group
+								className={classNames({'has-error': error})}
+							>
+								<label htmlFor={`${portletNamespace}file`}>
+									{Liferay.Language.get('file')}
 
-                                    {error && (
-                                        <ClayForm.FeedbackGroup>
-                                            <ClayForm.FeedbackItem>
-                                                <ClayForm.FeedbackIndicator symbol="exclamation-full" />
+									<ClayIcon
+										className="reference-mark"
+										symbol="asterisk"
+									/>
+								</label>
 
-                                                {error}
-                                            </ClayForm.FeedbackItem>
-                                        </ClayForm.FeedbackGroup>
-                                    )}
-                                </ClayForm.Group>
+								<ClayInput
+									data-testid={`${portletNamespace}file`}
+									id={`${portletNamespace}file`}
+									name={`${portletNamespace}file`}
+									onChange={validateFile}
+									required
+									type="file"
+								/>
 
-                                <ClayCheckbox
-                                    checked={overwrite}
-                                    data-testid={`${portletNamespace}overwrite`}
-                                    id={`${portletNamespace}overwrite`}
-                                    label={Liferay.Language.get(
-                                        'overwrite-existing-entries'
-                                    )}
-                                    name={`${portletNamespace}overwrite`}
-                                    onChange={() => setOverwrite((val) => !val)}
-                                />
-                            
-                        </form>
-                    </ClayLayout.Sheet>
-                )}
-            </ClayLayout.ContainerFluid>
-        </div>
+								{error && (
+									<ClayForm.FeedbackGroup>
+										<ClayForm.FeedbackItem>
+											<ClayForm.FeedbackIndicator symbol="exclamation-full" />
+
+											{error}
+										</ClayForm.FeedbackItem>
+									</ClayForm.FeedbackGroup>
+								)}
+							</ClayForm.Group>
+
+							<ClayCheckbox
+								checked={overwrite}
+								data-testid={`${portletNamespace}overwrite`}
+								id={`${portletNamespace}overwrite`}
+								label={Liferay.Language.get(
+									'overwrite-existing-entries'
+								)}
+								name={`${portletNamespace}overwrite`}
+								onChange={() => setOverwrite((val) => !val)}
+							/>
+						</form>
+					</ClayLayout.Sheet>
+				)}
+			</ClayLayout.ContainerFluid>
+		</div>
 	);
 }
 
--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/ImportResults.tsx
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/ImportResults.tsx
@@ -19,161 +19,184 @@ import {sub} from 'frontend-js-web';
 import React, {useState} from 'react';
 
 interface ImportResult {
-    message: string;
-    name: string;
-    type: "fragment" | "composition";
-
+	message: string;
+	name: string;
+	type: 'fragment' | 'composition';
 }
 
 export interface ImportResultsData {
-    imported: ImportResult[];
-	"imported-draft": ImportResult[];
-	invalid: ImportResult[];
+	'imported': ImportResult[];
+	'imported-draft': ImportResult[];
+	'invalid': ImportResult[];
 }
 
 interface Props {
-    fileName: string | null;
-    importResults: ImportResultsData;
+	fileName: string | null;
+	importResults: ImportResultsData;
 }
 
 function ImportResults({fileName, importResults}: Props) {
-
-    const [expanded, setExpanded] = useState(!importResults["imported-draft"] && !importResults.invalid);
+	const [expanded, setExpanded] = useState(
+		!importResults['imported-draft'] && !importResults.invalid
+	);
 
 	return (
-        <>
-            {importResults.imported && (
-                <ClayLayout.Sheet size='lg'>
-                        <ClayPanel
-                            collapsable
-                            displayTitle={
-                                <ClayPanel.Title>
-                                    <ClayIcon className="text-4 text-success"symbol="check-circle-full" />
+		<>
+			{importResults.imported && (
+				<ClayLayout.Sheet size="lg">
+					<ClayPanel
+						collapsable
+						displayTitle={
+							<ClayPanel.Title>
+								<ClayIcon
+									className="text-4 text-success"
+									symbol="check-circle-full"
+								/>
 
-                                    <span className="c-ml-3 font-weight-semi-bold text-4 text-success">
-                                        {sub(
-                                            Liferay.Language.get('x-items-were-imported'),
-                                            importResults.imported.length
-                                        )}
-                                    </span>
-                                </ClayPanel.Title>
-                            }
-                            expanded={expanded}
-                            onExpandedChange={() => {setExpanded(!expanded);}}
-                            showCollapseIcon
-                        >
-                            <ClayPanel.Body>
-                                <ul className="list-group sidebar-list-group">	
-                                    {importResults.imported.map((result, index) => (
-                                        <li
-                                            className="list-group-item list-group-item-flex p-0"
-                                            key={index}
-                                        >
-                                            <ClayLayout.ContentCol expand>
-                                                <div className="list-group-title">
-                                                    {result.name}
-                                                </div>
-                                            </ClayLayout.ContentCol>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </ClayPanel.Body>
-                        </ClayPanel>
-                </ClayLayout.Sheet>
-            )}
+								<span className="c-ml-3 font-weight-semi-bold text-4 text-success">
+									{sub(
+										Liferay.Language.get(
+											'x-items-were-imported'
+										),
+										importResults.imported.length
+									)}
+								</span>
+							</ClayPanel.Title>
+						}
+						expanded={expanded}
+						onExpandedChange={() => {
+							setExpanded(!expanded);
+						}}
+						showCollapseIcon
+					>
+						<ClayPanel.Body>
+							<ul className="list-group sidebar-list-group">
+								{importResults.imported.map((result, index) => (
+									<li
+										className="list-group-item list-group-item-flex p-0"
+										key={index}
+									>
+										<ClayLayout.ContentCol expand>
+											<div className="list-group-title">
+												{result.name}
+											</div>
+										</ClayLayout.ContentCol>
+									</li>
+								))}
+							</ul>
+						</ClayPanel.Body>
+					</ClayPanel>
+				</ClayLayout.Sheet>
+			)}
 
-            {importResults["imported-draft"] && (
-                <ClayLayout.Sheet size='lg'>
-                    <ClayPanel
-                        displayTitle={
-                            <ClayPanel.Title>
-                                <ClayIcon className="text-4 text-warning"symbol="warning-full" />
+			{importResults['imported-draft'] && (
+				<ClayLayout.Sheet size="lg">
+					<ClayPanel
+						displayTitle={
+							<ClayPanel.Title>
+								<ClayIcon
+									className="text-4 text-warning"
+									symbol="warning-full"
+								/>
 
-                                <span className="c-ml-3 font-weight-semi-bold text-4 text-warning">
-                                    {sub(
-                                        Liferay.Language.get('x-items-were-imported-with-warnings'),
-                                        importResults["imported-draft"].length
-                                    )}
-                                </span>
-                            </ClayPanel.Title>
-                            }
-                    >
-                        <ClayPanel.Body>
-                            <ul className="list-group sidebar-list-group">
-                                {importResults["imported-draft"].map((result, index) => (
-                                    <li
-                                        className="list-group-item list-group-item-flex p-0"
-                                        key={index}
-                                    >
-                                        <ClayLayout.ContentCol expand>
-                                            <div className="list-group-title">
-                                                {result.name}
-                                            </div>
+								<span className="c-ml-3 font-weight-semi-bold text-4 text-warning">
+									{sub(
+										Liferay.Language.get(
+											'x-items-were-imported-with-warnings'
+										),
+										importResults['imported-draft'].length
+									)}
+								</span>
+							</ClayPanel.Title>
+						}
+					>
+						<ClayPanel.Body>
+							<ul className="list-group sidebar-list-group">
+								{importResults['imported-draft'].map(
+									(result, index) => (
+										<li
+											className="list-group-item list-group-item-flex p-0"
+											key={index}
+										>
+											<ClayLayout.ContentCol expand>
+												<div className="list-group-title">
+													{result.name}
+												</div>
 
-                                            <div className="list-group-subtext text-warning">
-                                                {result.message}
-                                            </div>
-                                        </ClayLayout.ContentCol>
+												<div className="list-group-subtext text-warning">
+													{result.message}
+												</div>
+											</ClayLayout.ContentCol>
 
-                                        <ClayLayout.ContentCol>
-                                            <div className="list-group-subtitle">
-                                                {fileName}
-                                            </div>
-                                        </ClayLayout.ContentCol>
-                                    </li>
-                                ))}
-                            </ul>
-                        </ClayPanel.Body>
-                    </ClayPanel>
-                </ClayLayout.Sheet>
-            )}
+											<ClayLayout.ContentCol>
+												<div className="list-group-subtitle">
+													{fileName}
+												</div>
+											</ClayLayout.ContentCol>
+										</li>
+									)
+								)}
+							</ul>
+						</ClayPanel.Body>
+					</ClayPanel>
+				</ClayLayout.Sheet>
+			)}
 
-            {importResults.invalid && (
-                <ClayLayout.Sheet size='lg'>
-                    <ClayPanel
-                        displayTitle={
-                            <ClayPanel.Title>
-                                <ClayIcon className="text-4 text-danger" symbol="exclamation-full" />
+			{importResults.invalid && (
+				<ClayLayout.Sheet size="lg">
+					<ClayPanel
+						displayTitle={
+							<ClayPanel.Title>
+								<ClayIcon
+									className="text-4 text-danger"
+									symbol="exclamation-full"
+								/>
 
-                                <span className="c-ml-3 font-weight-semi-bold text-4 text-danger">
-                                    {sub(
-                                        Liferay.Language.get('x-items-could-not-be-imported'),
-                                        importResults.invalid.length
-                                    )}
-                                </span>
-                            </ClayPanel.Title>
-                        }
-                    >
-                        <ClayPanel.Body>
-                            <ul className="list-group sidebar-list-group">
-                                {importResults.invalid.map((result, index) => (
-                                    <li
-                                        className="list-group-item list-group-item-flex p-0"
-                                        key={index}
-                                    >
-                                        <ClayLayout.ContentCol expand>
-                                            <div className="list-group-title">
-                                                {result.name}
-                                            </div>
+								<span className="c-ml-3 font-weight-semi-bold text-4 text-danger">
+									{sub(
+										Liferay.Language.get(
+											'x-items-could-not-be-imported'
+										),
+										importResults.invalid.length
+									)}
+								</span>
+							</ClayPanel.Title>
+						}
+					>
+						<ClayPanel.Body>
+							<ul className="list-group sidebar-list-group">
+								{importResults.invalid.map((result, index) => (
+									<li
+										className="list-group-item list-group-item-flex p-0"
+										key={index}
+									>
+										<ClayLayout.ContentCol expand>
+											<div className="list-group-title">
+												{result.name}
+											</div>
 
-                                            <div className="list-group-subtext text-danger">
-                                                {result.message ? result.message : Liferay.Language.get('the-definition-of-the-item-is-invalid')}
-                                            </div>
-                                        </ClayLayout.ContentCol>
+											<div className="list-group-subtext text-danger">
+												{result.message
+													? result.message
+													: Liferay.Language.get(
+															'the-definition-of-the-item-is-invalid'
+													  )}
+											</div>
+										</ClayLayout.ContentCol>
 
-                                        <ClayLayout.ContentCol>
-                                            <div className="list-group-subtitle">
-                                                {fileName}
-                                            </div>
-                                        </ClayLayout.ContentCol>
-                                    </li>
-                                ))}
-                            </ul>
-                        </ClayPanel.Body>
-                    </ClayPanel>
-                </ClayLayout.Sheet>
-            )}
-        </>
+										<ClayLayout.ContentCol>
+											<div className="list-group-subtitle">
+												{fileName}
+											</div>
+										</ClayLayout.ContentCol>
+									</li>
+								))}
+							</ul>
+						</ClayPanel.Body>
+					</ClayPanel>
+				</ClayLayout.Sheet>
+			)}
+		</>
 	);
 }
 
