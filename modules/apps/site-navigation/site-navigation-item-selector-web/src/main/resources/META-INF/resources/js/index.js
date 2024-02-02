0ccd172daf102b8ca19d21379993597a71545bbd
Victor Galan <victor.galan@liferay.com>
2024-02-02 12:18:44 +0100

    LPD-16424 Typo

--- modules/apps/site-navigation/site-navigation-item-selector-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/site-navigation/site-navigation-item-selector-web/src/main/resources/META-INF/resources/js/index.js
@@ -3,5 +3,5 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export {defaut as SelectEntityHandler} from './SelectEntityHandler';
-export {defaut as SelectSiteNavigationMenuItem} from './SelectSiteNavigationMenuItem';
+export {default as SelectEntityHandler} from './SelectEntityHandler';
+export {default as SelectSiteNavigationMenuItem} from './SelectSiteNavigationMenuItem';
