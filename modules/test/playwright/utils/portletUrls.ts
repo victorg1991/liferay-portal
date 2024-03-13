b9d1ad634e8d0951f1ec294f88c8937a379fc62a
Sandro Chinea <sandrochinea@gmail.com>
2024-03-13 10:18:55 +0000

    LPD-XXX Add PORLET_URLS constant

---
+++ modules/test/playwright/utils/portletUrls.ts
@@ -0,0 +1,15 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+export const PORTLET_URLS = {
+	documentLibrary:
+		'/~/control_panel/manage?p_p_id=com_liferay_document_library_web_portlet_DLAdminPortlet',
+	journal:
+		'/~/control_panel/manage?p_p_id=com_liferay_journal_web_portlet_JournalPortlet',
+	knowledgeBase:
+		'/~/control_panel/manage?p_p_id=com_liferay_knowledge_base_web_portlet_AdminPortlet',
+	lockedItems:
+		'/~/control_panel/manage?p_p_id=com_liferay_locked_items_web_internal_portlet_LockedItemsPortlet',
+	pages: '/~/control_panel/manage?p_p_id=com_liferay_layout_admin_web_portlet_GroupPagesPortlet',
+};
