4261a2ee7701492d2470ee123f6aff07589f6300
Marco Leo <marco.leo@liferay.com>
2024-05-15 10:10:39 -0700

    LPD-25880 Add TestInfo annotation

---
+++ portal-test/src/com/liferay/portal/kernel/test/TestInfo.java
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.test;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Marco Leo
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface TestInfo {
+
+	public String[] value();
+
+}
