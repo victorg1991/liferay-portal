a5665e1272d66f6f2fc997e20111bba22da2bf4e
Georgel Pop <georgel.pop@liferay.com>
2024-08-28 11:06:29 +0200

    LPD-34570 Remove CUT_ITEM action type is not needed

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/undo/getActionLabel.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/undo/getActionLabel.js
@@ -12,7 +12,6 @@ import {
 	ADD_RULE,
 	CHANGE_MASTER_LAYOUT,
 	COPY_ITEM,
-	CUT_ITEM,
 	DELETE_ITEM,
 	DELETE_RULE,
 	DUPLICATE_ITEM,
@@ -73,8 +72,6 @@ export default function getActionLabel(
 			return sub(Liferay.Language.get('copy-x'), action.itemName);
 		case PASTE_ITEM:
 			return sub(Liferay.Language.get('paste-x'), action.itemName);
-		case CUT_ITEM:
-			return sub(Liferay.Language.get('cut-x'), action.itemName);
 		case MOVE_ITEM:
 			return sub(Liferay.Language.get('move-x'), action.itemName);
 		case SELECT_SEGMENTS_EXPERIENCE:
