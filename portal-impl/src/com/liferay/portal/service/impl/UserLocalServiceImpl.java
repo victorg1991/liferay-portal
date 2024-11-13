bd45a16b0f11b7750d72caeaf9118f9ffaf7ec38
ChrisKian <christopher.kian@liferay.com>
2024-11-12 17:29:12 -0800

    LPD-36309 Non-LDAP users should have -1 as their ldapServerId value

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -4807,7 +4807,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			}
 
 			validate(
-				companyId, 0, user.getUserId(), autoPassword, password1,
+				companyId, -1, user.getUserId(), autoPassword, password1,
 				password2, autoScreenName, screenName, emailAddress, null,
 				firstName, middleName, lastName, null, locale);
 
@@ -7022,7 +7022,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		throws PortalException {
 
 		validate(
-			companyId, 0, userId, autoPassword, password1, password2,
+			companyId, -1, userId, autoPassword, password1, password2,
 			autoScreenName, screenName, emailAddress, openId, firstName,
 			middleName, lastName, organizationIds, locale);
 	}
