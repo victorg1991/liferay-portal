73fbf5a946d6ea0ba86d09704ae5d6dc791fdc62
Brian Chan <brian.chan@liferay.com>
2024-11-13 23:52:49 -0600

    LPD-41878 Make it more explicit

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
@@ -2781,7 +2781,7 @@ public class BundleSiteInitializerTest {
 
 		pageDefinitionString = String.valueOf(sitePage.getPageDefinition());
 
-		Assert.assertFalse(pageDefinitionString.contains("$CLASS_NAME_ID"));
+		Assert.assertFalse(pageDefinitionString.contains("[$CLASS_NAME_ID:"));
 	}
 
 	private void _assertPublicLayouts2() throws Exception {
