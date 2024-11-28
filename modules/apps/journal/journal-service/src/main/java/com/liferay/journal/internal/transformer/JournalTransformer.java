bed90c359ffe5ea23066724ef353320f358f13c1
Roberto Díaz <roberto.diaz@liferay.com>
2024-11-28 10:37:43 +0100

    LPD-43080 Escape field

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/transformer/JournalTransformer.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/transformer/JournalTransformer.java
@@ -61,6 +61,7 @@ import com.liferay.portal.kernel.templateparser.TransformerListener;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
@@ -492,6 +493,9 @@ public class JournalTransformer {
 			}
 
 			data = JSONUtil.toString(dataJSONArray);
+		}
+		else if (type.equals(DDMFormFieldTypeConstants.TEXT)) {
+			data = HtmlUtil.escape(data);
 		}
 
 		if (dynamicContentElement != null) {
