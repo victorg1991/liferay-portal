6ed0cd0a346c1e558168ba2315e0d2133db4ffeb
Rafael Praxedes <rafael.praxedes@liferay.com>
2024-12-18 14:34:42 -0300

    LPD-36309 SF

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1286,15 +1286,15 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		user.setPasswordEncrypted(true);
 
+		boolean passwordReset = false;
+
 		if ((ldapServerId <= 0) ||
 			!LDAPSettingsUtil.isPasswordPolicyEnabled(companyId)) {
 
-			user.setPasswordReset(_isPasswordReset(companyId));
-		}
-		else {
-			user.setPasswordReset(false);
+			passwordReset = _isPasswordReset(companyId);
 		}
 
+		user.setPasswordReset(passwordReset);
 		user.setScreenName(screenName);
 		user.setEmailAddress(emailAddress);
 
