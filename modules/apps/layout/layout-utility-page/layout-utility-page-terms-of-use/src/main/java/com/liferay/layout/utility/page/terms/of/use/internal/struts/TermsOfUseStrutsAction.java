1f1635ebf24dd6b3e7dbfcd06934c806caf82591
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-12-31 13:31:36 +0100

    LPD-1226 Apply the same fix on the TermsOfUseStrutsAction to prevent a NPE in case no element with ID "content"
    (cherry picked from commit 950cc4a1dab689d313cbd5e219d5603f96519c4f)

--- modules/apps/layout/layout-utility-page/layout-utility-page-terms-of-use/src/main/java/com/liferay/layout/utility/page/terms/of/use/internal/struts/TermsOfUseStrutsAction.java
+++ modules/apps/layout/layout-utility-page/layout-utility-page-terms-of-use/src/main/java/com/liferay/layout/utility/page/terms/of/use/internal/struts/TermsOfUseStrutsAction.java
@@ -6,6 +6,9 @@
 package com.liferay.layout.utility.page.terms.of.use.internal.struts;
 
 import com.liferay.petra.io.unsync.UnsyncStringWriter;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.service.LayoutSetLocalService;
 import com.liferay.portal.kernel.servlet.PipingServletResponse;
@@ -73,14 +76,29 @@ public class TermsOfUseStrutsAction implements StrutsAction {
 				httpServletResponse, "portal_normal.ftl", layoutSet.getTheme(),
 				false));
 
-		Element contentElement = document.getElementById("content");
+		Element element = document.getElementById("content");
 
-		contentElement.html(unsyncStringWriter.toString());
+		if (element == null) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					StringBundler.concat(
+						"Theme ", layoutSet.getThemeId(),
+						" lacks a tag with ID 'content', replacing all body ",
+						"content."));
+			}
+
+			element = document.body();
+		}
+
+		element.html(unsyncStringWriter.toString());
 
 		ServletResponseUtil.write(httpServletResponse, document.html());
 
 		return null;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		TermsOfUseStrutsAction.class);
 
 	@Reference
 	private LayoutSetLocalService _layoutSetLocalService;
