4966b5a7a86be38a15c5485ae51066e8a1b9df65
ChrisKian <christopher.kian@liferay.com>
2025-01-06 10:44:01 -0800

    LPD-26723 Use WebKeys instead

--- modules/dxp/apps/saml/saml-impl/src/main/java/com/liferay/saml/internal/servlet/filter/SpSessionTerminationSamlPortalFilter.java
+++ modules/dxp/apps/saml/saml-impl/src/main/java/com/liferay/saml/internal/servlet/filter/SpSessionTerminationSamlPortalFilter.java
@@ -7,6 +7,7 @@ package com.liferay.saml.internal.servlet.filter;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.saml.helper.SamlHttpRequestHelper;
 import com.liferay.saml.persistence.model.SamlSpSession;
@@ -56,7 +57,7 @@ public class SpSessionTerminationSamlPortalFilter extends BaseSamlPortalFilter {
 		HttpServletRequest httpServletRequest,
 		HttpServletResponse httpServletResponse) {
 
-		if (httpServletRequest.getAttribute("COMPANY_ID") == null) {
+		if (httpServletRequest.getAttribute(WebKeys.COMPANY_ID) == null) {
 			PortalInstances.getCompanyId(httpServletRequest);
 		}
 
