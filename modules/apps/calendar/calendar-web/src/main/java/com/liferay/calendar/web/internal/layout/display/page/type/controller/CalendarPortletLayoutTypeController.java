dee50232b50502094b04ee6977e8e96a60bcef0e
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-03-20 13:54:07 -0300

    LPD-51804 Implement CalendarPortletLayoutTypeController similar to DDMFormPortletLayoutTypeController

---
+++ modules/apps/calendar/calendar-web/src/main/java/com/liferay/calendar/web/internal/layout/display/page/type/controller/CalendarPortletLayoutTypeController.java
@@ -0,0 +1,112 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.calendar.web.internal.layout.display.page.type.controller;
+
+import com.liferay.calendar.constants.CalendarPortletKeys;
+import com.liferay.layout.type.controller.BaseLayoutTypeControllerImpl;
+import com.liferay.petra.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutTypeController;
+import com.liferay.portal.kernel.servlet.PipingServletResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carolina Barbosa
+ */
+@Component(
+	property = "layout.type=" + CalendarPortletKeys.CALENDAR,
+	service = LayoutTypeController.class
+)
+public class CalendarPortletLayoutTypeController
+	extends BaseLayoutTypeControllerImpl {
+
+	@Override
+	public String getType() {
+		return LayoutConstants.TYPE_PORTLET;
+	}
+
+	@Override
+	public String getURL() {
+		return _URL;
+	}
+
+	@Override
+	public boolean isBrowsable() {
+		return false;
+	}
+
+	@Override
+	public boolean isFirstPageable() {
+		return true;
+	}
+
+	@Override
+	public boolean isFullPageDisplayable() {
+		return false;
+	}
+
+	@Override
+	public boolean isInstanceable() {
+		return false;
+	}
+
+	@Override
+	public boolean isParentable() {
+		return false;
+	}
+
+	@Override
+	public boolean isSitemapable() {
+		return false;
+	}
+
+	@Override
+	public boolean isURLFriendliable() {
+		return true;
+	}
+
+	@Override
+	protected ServletResponse createServletResponse(
+		HttpServletResponse httpServletResponse,
+		UnsyncStringWriter unsyncStringWriter) {
+
+		return new PipingServletResponse(
+			httpServletResponse, unsyncStringWriter);
+	}
+
+	@Override
+	protected String getEditPage() {
+		return _EDIT_PAGE;
+	}
+
+	@Override
+	protected ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	protected String getViewPage() {
+		return _VIEW_PAGE;
+	}
+
+	private static final String _EDIT_PAGE = "/layout/edit/portlet.jsp";
+
+	private static final String _URL =
+		"${liferay:mainPath}/portal/layout?p_l_id=${liferay:plid}" +
+			"&p_p_state=pop_up&p_v_l_s_g_id=${liferay:pvlsgid}";
+
+	private static final String _VIEW_PAGE = "/layout/view/portlet.jsp";
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.calendar.web)")
+	private ServletContext _servletContext;
+
+}
