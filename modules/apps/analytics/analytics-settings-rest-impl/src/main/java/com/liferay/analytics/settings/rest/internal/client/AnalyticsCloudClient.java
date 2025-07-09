52dc8914bdad68a98b4e70660450123604c2c08d
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2025-07-09 12:56:33 -0700

    LPD-58516 add body on disconnect

--- modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/client/AnalyticsCloudClient.java
+++ modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/client/AnalyticsCloudClient.java
@@ -31,7 +31,6 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.search.Sort;
-import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.Base64;
 import com.liferay.portal.kernel.util.ContentTypes;
@@ -136,13 +135,18 @@ public class AnalyticsCloudClient {
 	}
 
 	public AnalyticsDataSource disconnectAnalyticsDataSource(
-			AnalyticsConfiguration analyticsConfiguration)
+			AnalyticsConfiguration analyticsConfiguration, Company company)
 		throws Exception {
 
 		try {
 			Http.Options options = _getOptions(analyticsConfiguration);
 
-			options.addHeader(HttpHeaders.CONTENT_LENGTH, "0");
+			options.addHeader("Content-Type", ContentTypes.APPLICATION_JSON);
+			options.setBody(
+				JSONUtil.put(
+					"url", company.getPortalURL(0)
+				).toString(),
+				ContentTypes.APPLICATION_JSON, StringPool.UTF8);
 			options.setLocation(
 				String.format(
 					"%s/api/1.0/data-sources/%s/disconnect",
