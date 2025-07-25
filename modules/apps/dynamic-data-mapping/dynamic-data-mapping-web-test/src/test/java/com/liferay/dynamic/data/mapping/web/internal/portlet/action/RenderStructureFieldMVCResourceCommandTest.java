473c010d65e7ada1f47b52a12e8309923749b026
mxs2 <mateus.xavier@liferay.com>
2025-07-25 15:43:55 -0300

    Revert "LPD-56406 Add test"
    This reverts commit 29689cec1df7c41974714fb6afcb0a3ac7836581.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web-test/src/test/java/com/liferay/dynamic/data/mapping/web/internal/portlet/action/RenderStructureFieldMVCResourceCommandTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web-test/src/test/java/com/liferay/dynamic/data/mapping/web/internal/portlet/action/RenderStructureFieldMVCResourceCommandTest.java
@@ -5,10 +5,6 @@
 
 package com.liferay.dynamic.data.mapping.web.internal.portlet.action;
 
-import com.liferay.dynamic.data.mapping.io.DDMFormDeserializer;
-import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeResponse;
-import com.liferay.dynamic.data.mapping.model.DDMForm;
-import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -21,10 +17,7 @@ import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.Collections;
-
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 
@@ -39,8 +32,11 @@ public class RenderStructureFieldMVCResourceCommandTest {
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
 
-	@Before
-	public void setUp() throws Exception {
+	@Test
+	public void testCreateDDMFormFieldRenderingContext() {
+		HttpServletRequest httpServletRequest = Mockito.mock(
+			HttpServletRequest.class);
+
 		ThemeDisplay themeDisplay = Mockito.mock(ThemeDisplay.class);
 
 		Mockito.when(
@@ -50,24 +46,23 @@ public class RenderStructureFieldMVCResourceCommandTest {
 		);
 
 		Mockito.when(
-			_httpServletRequest.getAttribute(WebKeys.THEME_DISPLAY)
+			httpServletRequest.getAttribute(WebKeys.THEME_DISPLAY)
 		).thenReturn(
 			themeDisplay
 		);
-	}
 
-	@Test
-	public void testCreateDDMFormFieldRenderingContext() {
+		String script = "'\"></option><img onerror=alert(123) src=x>";
+
 		Mockito.when(
-			_httpServletRequest.getParameter("namespace")
+			httpServletRequest.getParameter("namespace")
 		).thenReturn(
-			_SCRIPT
+			script
 		);
 
 		Mockito.when(
-			_httpServletRequest.getParameter("portletNamespace")
+			httpServletRequest.getParameter("portletNamespace")
 		).thenReturn(
-			_SCRIPT
+			script
 		);
 
 		RenderStructureFieldMVCResourceCommand
@@ -80,82 +75,17 @@ public class RenderStructureFieldMVCResourceCommandTest {
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext =
 			renderStructureFieldMVCResourceCommand.
 				createDDMFormFieldRenderingContext(
-					_httpServletRequest,
+					httpServletRequest,
 					Mockito.mock(HttpServletResponse.class));
 
 		Assert.assertEquals(
-			HtmlUtil.escapeAttribute(_SCRIPT),
+			HtmlUtil.escapeAttribute(script),
 			ddmFormFieldRenderingContext.getNamespace());
 		Assert.assertEquals(
-			HtmlUtil.escapeAttribute(_SCRIPT),
+			HtmlUtil.escapeAttribute(script),
 			ddmFormFieldRenderingContext.getPortletNamespace());
 	}
 
-	@Test
-	public void testGetDDMFormField() {
-		Mockito.when(
-			_httpServletRequest.getParameter("fieldName")
-		).thenReturn(
-			HtmlUtil.escapeAttribute(_SCRIPT)
-		);
-
-		DDMFormDeserializer ddmFormDeserializer = Mockito.mock(
-			DDMFormDeserializer.class);
-
-		DDMFormDeserializerDeserializeResponse
-			ddmFormDeserializerDeserializeResponse = Mockito.mock(
-				DDMFormDeserializerDeserializeResponse.class);
-
-		Mockito.when(
-			ddmFormDeserializer.deserialize(Mockito.any())
-		).thenReturn(
-			ddmFormDeserializerDeserializeResponse
-		);
-
-		DDMForm ddmForm = Mockito.mock(DDMForm.class);
-
-		Mockito.when(
-			ddmFormDeserializerDeserializeResponse.getDDMForm()
-		).thenReturn(
-			ddmForm
-		);
-
-		DDMFormField mockDDMFormField = Mockito.mock(DDMFormField.class);
-
-		Mockito.when(
-			mockDDMFormField.getName()
-		).thenReturn(
-			HtmlUtil.escapeAttribute(_SCRIPT)
-		);
-
-		Mockito.when(
-			ddmForm.getDDMFormFieldsMap(true)
-		).thenReturn(
-			Collections.singletonMap(
-				HtmlUtil.escapeAttribute(_SCRIPT), mockDDMFormField)
-		);
-
-		RenderStructureFieldMVCResourceCommand
-			renderStructureFieldMVCResourceCommand =
-				new RenderStructureFieldMVCResourceCommand();
-
-		ReflectionTestUtil.setFieldValue(
-			renderStructureFieldMVCResourceCommand, "_jsonDDMFormDeserializer",
-			ddmFormDeserializer);
-
-		DDMFormField ddmFormField =
-			renderStructureFieldMVCResourceCommand.getDDMFormField(
-				_httpServletRequest);
-
-		Assert.assertEquals(
-			HtmlUtil.escapeAttribute(_SCRIPT), ddmFormField.getName());
-	}
-
-	private static final String _SCRIPT =
-		"'\"></option><img onerror=alert(123) src=x>";
-
-	private final HttpServletRequest _httpServletRequest = Mockito.mock(
-		HttpServletRequest.class);
 	private final Portal _portal = Mockito.mock(Portal.class);
 
 }
