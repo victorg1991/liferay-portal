13312566c67b7955766b6e99b64b29dcfbc834e5
Olivia Yu <olivia.yu@liferay.com>
2025-10-15 15:23:28 -0700

    LPD-68675 portal-search-web: Use layout friendly URL instead of currentURL for proper redirect

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactory.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactory.java
@@ -21,7 +21,6 @@ import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -93,7 +92,7 @@ public class SearchBarPortletDisplayContextFactory {
 
 		if (Validator.isBlank(destination)) {
 			searchBarPortletDisplayContext.setSearchURL(
-				_getURLCurrentPath(themeDisplay));
+				getLayoutFriendlyURL(themeDisplay.getLayout(), themeDisplay));
 		}
 		else {
 			String destinationURL = _getDestinationURL(
@@ -414,10 +413,6 @@ public class SearchBarPortletDisplayContextFactory {
 
 		return portletSharedSearchResponse.getFederatedSearchResponse(
 			searchBarPortletPreferences.getFederatedSearchKey());
-	}
-
-	private String _getURLCurrentPath(ThemeDisplay themeDisplay) {
-		return HttpComponentsUtil.getPath(themeDisplay.getURLCurrent());
 	}
 
 	private boolean _isEmptySearchEnabled(
