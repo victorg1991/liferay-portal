16e284a660e443a6e6a4d00494e0ee6a3230343b
Georgel Pop <georgel.pop@liferay.com>
2025-10-24 13:41:22 +0200

    LPD-69505 Add _ddmPermissionSupport permission checker for each action ADD, DELETE and UPDATE. Use _ddmTemplateService instead of _ddmTemplateLocalService to check permission

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/action/AddTemplateEntryMVCActionCommand.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/action/AddTemplateEntryMVCActionCommand.java
@@ -9,7 +9,7 @@ import com.liferay.dynamic.data.mapping.constants.DDMTemplateConstants;
 import com.liferay.dynamic.data.mapping.exception.TemplateNameException;
 import com.liferay.dynamic.data.mapping.exception.TemplateScriptException;
 import com.liferay.dynamic.data.mapping.model.DDMTemplate;
-import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateService;
 import com.liferay.info.item.provider.InfoItemFormProvider;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseTransactionalMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.template.TemplateConstants;
@@ -83,8 +84,8 @@ public class AddTemplateEntryMVCActionCommand
 			DDMTemplate.class.getName(), actionRequest);
 
 		try {
-			DDMTemplate ddmTemplate = _ddmTemplateLocalService.addTemplate(
-				themeDisplay.getUserId(), serviceContext.getScopeGroupId(),
+			DDMTemplate ddmTemplate = _ddmTemplateService.addTemplate(
+				serviceContext.getScopeGroupId(),
 				_portal.getClassNameId(TemplateEntry.class), 0,
 				_portal.getClassNameId(TemplateEntry.class), nameMap,
 				Collections.emptyMap(),
@@ -142,6 +143,15 @@ public class AddTemplateEntryMVCActionCommand
 				_language.get(
 					themeDisplay.getLocale(), "please-enter-a-valid-script"));
 		}
+		else if (portalException instanceof
+					PrincipalException.MustHavePermission) {
+
+			return JSONUtil.put(
+				"other",
+				_language.get(
+					themeDisplay.getLocale(),
+					"you-do-not-have-the-required-permissions"));
+		}
 
 		if (_log.isDebugEnabled()) {
 			_log.debug(portalException);
@@ -170,7 +180,7 @@ public class AddTemplateEntryMVCActionCommand
 		AddTemplateEntryMVCActionCommand.class);
 
 	@Reference
-	private DDMTemplateLocalService _ddmTemplateLocalService;
+	private DDMTemplateService _ddmTemplateService;
 
 	@Reference
 	private Language _language;
