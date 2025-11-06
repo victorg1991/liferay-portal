a9bc60764e727c540e0ab84d7094322878d9050a
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2025-11-06 13:28:16 +0400

    LPD-70011 - Duplicate pkceChallge for oauth2 bundle

--- modules/apps/oauth2-provider/oauth2-provider-web/src/main/resources/META-INF/resources/js/OAuth2Client.ts
+++ modules/apps/oauth2-provider/oauth2-provider-web/src/main/resources/META-INF/resources/js/OAuth2Client.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {pkceChallenge} from 'frontend-js-web';
+import pkceChallenge from './pkceChallenge';
 
 interface IOAuth2ClientFromParametersOptions {
 	authorizeURL?: string;
