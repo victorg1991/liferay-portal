9dbeb0a70eb66302a279ce93b9569cc5006a4a49
balazssk <balazs.safrany-kovalik@liferay.com>
2025-11-24 11:41:06 +0100

    LPD-72320 Test

---
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/internal/util/test/JournalDefaultTemplateProviderTest.java
@@ -0,0 +1,85 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.journal.internal.util.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.DDMFormFieldType;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.storage.StorageType;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormTestUtil;
+import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestHelper;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.util.JournalDefaultTemplateProvider;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Balázs Sáfrány-Kovalik
+ */
+@RunWith(Arquillian.class)
+public class JournalDefaultTemplateProviderTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_ddmStructureTestHelper = new DDMStructureTestHelper(
+			_portal.getClassNameId(JournalArticle.class), _group);
+	}
+
+	@Test
+	public void testGetScript() throws Exception {
+		_testGetScriptWithImageField();
+	}
+
+	private void _testGetScriptWithImageField() throws Exception {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm();
+
+		DDMFormField ddmFormField = DDMFormTestUtil.createDDMFormField(
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			DDMFormFieldType.IMAGE, "string", true, false, false);
+
+		ddmForm.addDDMFormField(ddmFormField);
+
+		DDMStructure ddmStructure = _ddmStructureTestHelper.addStructure(
+			ddmForm, StorageType.DEFAULT.getValue());
+
+		String script = _journalDefaultTemplateProvider.getScript(
+			ddmStructure.getStructureId());
+
+		Assert.assertTrue(
+			script.contains("<img alt=\"${htmlUtil.escapeAttribute("));
+	}
+
+	private DDMStructureTestHelper _ddmStructureTestHelper;
+	private Group _group;
+
+	@Inject
+	private JournalDefaultTemplateProvider _journalDefaultTemplateProvider;
+
+	@Inject
+	private Portal _portal;
+
+}
