80d4086e923b84cd05389c4da59bb96247f334c0
Marco Galluzzi <marco.galluzzi@liferay.com>
2026-01-30 18:16:46 +0000

    LPD-77450 Improve check to exclude NaN value

--- portal-impl/src/com/liferay/portlet/ratings/service/impl/RatingsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/ratings/service/impl/RatingsEntryLocalServiceImpl.java
@@ -252,7 +252,7 @@ public class RatingsEntryLocalServiceImpl
 	}
 
 	protected void validate(double score) throws PortalException {
-		if ((score > 1) || (score < 0)) {
+		if ((score > 1) || (score < 0) || Double.isNaN(score)) {
 			throw new EntryScoreException(
 				"Score " + score + " is not a value between 0 and 1");
 		}
