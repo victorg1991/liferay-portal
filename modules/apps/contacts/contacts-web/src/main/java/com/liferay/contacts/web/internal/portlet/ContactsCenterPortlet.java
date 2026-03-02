099bc945c9eb7ab665b4306e3e127fe1b1f2c301
Tancredi Covioli <tancredi.covioli@liferay.com>
2026-03-02 11:30:25 +0100

    LPD-79852 Adds permission checks on Entry update

--- modules/apps/contacts/contacts-web/src/main/java/com/liferay/contacts/web/internal/portlet/ContactsCenterPortlet.java
+++ modules/apps/contacts/contacts-web/src/main/java/com/liferay/contacts/web/internal/portlet/ContactsCenterPortlet.java
@@ -13,6 +13,7 @@ import com.liferay.contacts.exception.DuplicateEntryEmailAddressException;
 import com.liferay.contacts.exception.EntryEmailAddressException;
 import com.liferay.contacts.model.Entry;
 import com.liferay.contacts.service.EntryLocalService;
+import com.liferay.contacts.service.EntryService;
 import com.liferay.contacts.util.ContactsUtil;
 import com.liferay.contacts.web.internal.constants.ContactsPortletKeys;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
@@ -478,7 +479,7 @@ public class ContactsCenterPortlet extends MVCPortlet {
 			Entry entry = null;
 
 			if (entryId > 0) {
-				entry = entryLocalService.getEntry(entryId);
+				entry = entryService.getEntry(entryId);
 
 				if (entry.getUserId() == themeDisplay.getUserId()) {
 					entry = entryLocalService.updateEntry(
@@ -719,6 +720,9 @@ public class ContactsCenterPortlet extends MVCPortlet {
 
 	@Reference
 	protected EntryLocalService entryLocalService;
+
+	@Reference
+	protected EntryService entryService;
 
 	@Reference
 	protected File file;
