054a5fa0a5770325885a691f847bbd6436962278
Lucas Miranda <lucas.miranda@liferay.com>
2026-03-13 09:12:40 -0300

    LPD-81903 Add check before redirect

--- modules/apps/portal-settings/portal-settings-authentication-opensso-web/src/main/java/com/liferay/portal/settings/authentication/opensso/web/internal/portlet/action/TestOpenSSOMVCRenderCommand.java
+++ modules/apps/portal-settings/portal-settings-authentication-opensso-web/src/main/java/com/liferay/portal/settings/authentication/opensso/web/internal/portlet/action/TestOpenSSOMVCRenderCommand.java
@@ -10,6 +10,8 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.constants.MVCRenderConstants;
+import com.liferay.portal.kernel.security.auth.AuthTokenUtil;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.util.Portal;
 
 import javax.portlet.PortletException;
@@ -38,6 +40,20 @@ public class TestOpenSSOMVCRenderCommand implements MVCRenderCommand {
 	public String render(
 			RenderRequest renderRequest, RenderResponse renderResponse)
 		throws PortletException {
+
+		try {
+			AuthTokenUtil.checkCSRFToken(
+				_portal.getHttpServletRequest(renderRequest),
+				TestOpenSSOMVCRenderCommand.class.getName());
+		}
+		catch (PrincipalException principalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Invalid CSRF token", principalException);
+			}
+
+			throw new PortletException(
+				"Invalid security token", principalException);
+		}
 
 		RequestDispatcher requestDispatcher =
 			_servletContext.getRequestDispatcher(_JSP_PATH);
