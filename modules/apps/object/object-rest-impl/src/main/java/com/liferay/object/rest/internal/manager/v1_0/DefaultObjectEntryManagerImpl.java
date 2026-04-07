64b33cf5c92152174f28174b3eb3969264f82630
Victor Galan <victor.galan@liferay.com>
2026-04-07 09:58:13 +0200

    LPD-84657 Set the folder automatically only for the main object
    Related entries should not be listed in the CMS

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -17,8 +17,10 @@ import com.liferay.object.action.engine.ObjectActionEngine;
 import com.liferay.object.comment.ObjectEntryComment;
 import com.liferay.object.constants.ObjectActionTriggerConstants;
 import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.constants.ObjectEntryFolderConstants;
 import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.constants.ObjectFieldSettingConstants;
+import com.liferay.object.constants.ObjectFolderConstants;
 import com.liferay.object.constants.ObjectRelationshipConstants;
 import com.liferay.object.entry.folder.subscription.util.ObjectEntryFolderSubscriptionUtil;
 import com.liferay.object.entry.util.ObjectEntryDTOConverterUtil;
@@ -35,6 +37,7 @@ import com.liferay.object.model.ObjectEntryTable;
 import com.liferay.object.model.ObjectEntryVersion;
 import com.liferay.object.model.ObjectEntryVersionTable;
 import com.liferay.object.model.ObjectField;
+import com.liferay.object.model.ObjectFolder;
 import com.liferay.object.model.ObjectRelationship;
 import com.liferay.object.model.ObjectRelationshipModel;
 import com.liferay.object.related.models.ObjectRelatedModelsProvider;
@@ -1389,7 +1392,7 @@ public class DefaultObjectEntryManagerImpl
 						objectDefinition.getObjectDefinitionId(),
 						_getObjectEntryFolderId(
 							objectDefinition.getCompanyId(), groupId,
-							objectEntry, serviceContext),
+							objectEntry, objectDefinition, serviceContext),
 						_toObjectValues(
 							0L, dtoConverterContext.getLocale(),
 							objectDefinition, objectEntry, scopeKey,
@@ -1610,7 +1613,7 @@ public class DefaultObjectEntryManagerImpl
 				groupId, objectDefinition.getObjectDefinitionId(),
 				_getObjectEntryFolderId(
 					objectDefinition.getCompanyId(), groupId, objectEntry,
-					serviceContext),
+					objectDefinition, serviceContext),
 				objectEntry.getDefaultLanguageId(), values, serviceContext);
 
 		return _toObjectEntry(
@@ -1888,6 +1891,9 @@ public class DefaultObjectEntryManagerImpl
 		ServiceContext serviceContext = _createServiceContext(
 			dtoConverterContext, objectDefinition, objectEntry, scopeKey);
 
+		com.liferay.object.model.ObjectEntry parentServiceBuilderObjectEntry =
+			_objectEntryService.fetchObjectEntry(objectEntryId);
+
 		return _toObjectEntry(
 			dtoConverterContext, objectDefinition,
 			_addOrUpdateNestedObjectEntries(
@@ -1895,9 +1901,7 @@ public class DefaultObjectEntryManagerImpl
 				_getObjectRelationships(objectDefinition, objectEntry),
 				_objectEntryService.addObjectEntry(
 					groupId, objectDefinition.getObjectDefinitionId(),
-					_getObjectEntryFolderId(
-						objectDefinition.getCompanyId(), groupId, objectEntry,
-						serviceContext),
+					GetterUtil.getLong(objectEntry.getObjectEntryFolderId()),
 					objectEntry.getDefaultLanguageId(),
 					_toObjectValues(
 						objectField.getObjectFieldId(),
@@ -1905,7 +1909,7 @@ public class DefaultObjectEntryManagerImpl
 						objectEntry, scopeKey, serviceContext),
 					serviceContext),
 				scopeKey),
-			_objectEntryService.fetchObjectEntry(objectEntryId));
+			parentServiceBuilderObjectEntry);
 	}
 
 	private void _checkApprovedObjectEntry(
@@ -2382,14 +2386,52 @@ public class DefaultObjectEntryManagerImpl
 
 	private long _getObjectEntryFolderId(
 			long companyId, long groupId, ObjectEntry objectEntry,
-			ServiceContext serviceContext)
+			ObjectDefinition objectDefinition, ServiceContext serviceContext)
 		throws Exception {
 
 		String objectEntryFolderExternalReferenceCode =
 			objectEntry.getObjectEntryFolderExternalReferenceCode();
 
 		if (Validator.isNull(objectEntryFolderExternalReferenceCode)) {
-			return GetterUtil.getLong(objectEntry.getObjectEntryFolderId());
+			long objectEntryFolderId = GetterUtil.getLong(
+				objectEntry.getObjectEntryFolderId());
+
+			if ((objectEntryFolderId != 0) ||
+				!FeatureFlagManagerUtil.isEnabled(companyId, "LPD-17564") ||
+				(groupId == 0)) {
+
+				return objectEntryFolderId;
+			}
+
+			Group group = _groupLocalService.fetchGroup(groupId);
+
+			if ((group == null) || !group.isDepot()) {
+				return objectEntryFolderId;
+			}
+
+			DepotEntry depotEntry =
+				_depotEntryLocalService.fetchGroupDepotEntry(groupId);
+
+			if ((depotEntry == null) ||
+				(depotEntry.getType() != DepotConstants.TYPE_SPACE)) {
+
+				return objectEntryFolderId;
+			}
+
+			ObjectFolder objectFolder = objectDefinition.getObjectFolder();
+
+			if ((objectFolder != null) &&
+				Objects.equals(
+					objectFolder.getExternalReferenceCode(),
+					ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_FILE_TYPES)) {
+
+				objectEntryFolderExternalReferenceCode =
+					ObjectEntryFolderConstants.EXTERNAL_REFERENCE_CODE_FILES;
+			}
+			else {
+				objectEntryFolderExternalReferenceCode =
+					ObjectEntryFolderConstants.EXTERNAL_REFERENCE_CODE_CONTENTS;
+			}
 		}
 
 		ObjectEntryFolder objectEntryFolder =
@@ -3754,7 +3796,7 @@ public class DefaultObjectEntryManagerImpl
 					_getObjectEntryFolderId(
 						serviceBuilderObjectEntry.getCompanyId(),
 						serviceBuilderObjectEntry.getGroupId(), objectEntry,
-						serviceContext),
+						objectDefinition, serviceContext),
 					values, serviceContext);
 		}
 		else {
@@ -3763,7 +3805,7 @@ public class DefaultObjectEntryManagerImpl
 				_getObjectEntryFolderId(
 					serviceBuilderObjectEntry.getCompanyId(),
 					serviceBuilderObjectEntry.getGroupId(), objectEntry,
-					serviceContext),
+					objectDefinition, serviceContext),
 				values, serviceContext);
 		}
 
